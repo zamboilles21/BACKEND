03983125ca03bcdb9a1daf2f044f4bbf0a75389b
zamboilles21 <zamboilles@turr.hu>
2022-11-02 19:18:06 +0100

    javitas

--- chatAPP/server.js
+++ chatAPP/server.js
@@ -1,41 +1,40 @@
+require('dotenv').config();
 const express = require('express');
-
-const path = require('path');
-const ejs = require('ejs');
-const server=require('http').createServer(app);
-const socket=require('socket.io')(server);
-const config = require('./config.js');
 const app = express();
+const ejs = require('ejs');
+const path = require('path');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port = 3000;
 
+app.use('/assets', express.static(path.join(__dirname + '/assets')));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    ejs.renderFile('views/index.ejs', { config, hiba:message }, (err, data) => {
-        if (err) {
-            res.status(500).send(err);
-        } else {
-            res.status(200).send(data);
-        }
-    });
+    ejs.renderFile('views/index.ejs', (err, data) => {
+        res.send(data);
+    })
+})
+
+app.post('/chat', (req, res) => {
+    let user = {
+        nickname: req.body.nickname,
+        roomname: req.body.roomname
+    }
+
+    if (user.nickname == '' || user.roomname == '') {
+        res.redirect('/');
+    } else {
+        ejs.renderFile('views/chat.ejs', { user }, (err, data) => {
+            res.send(data);
+        })
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log(socket.id)
 });
 
-
-
-// Middlewares
-app.use('/assets', express.static(path.join(__dirname + '/assets')));
-app.use('/views', express.static(path.join(__dirname + '/views')));
-app.use(express.urlencoded({ extended: true }));
-app.use(session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true
-}));
-
-app.locals.message = '';
-app.locals.messagetype = 'danger';
-app.locals.isMessage = false;
-
-
-
-app.listen(config.appconfig.port, () => {
-    console.log(`Server listening on port ${config.appconfig.port}...`);
-});
+app.listen(port, () => {
+    console.log("Server running...");
+})
