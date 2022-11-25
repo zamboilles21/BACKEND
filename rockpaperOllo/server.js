1a48ec99266adbd5a269fc25ed1587c9fab81725
zamboilles21 <zamboilles@turr.hu>
2022-11-25 10:34:38 +0100

    gfhfghfg

--- rockpaperOllo/server.js
+++ rockpaperOllo/server.js
@@ -5,8 +5,13 @@ const io = require('socket.io')(server);
 const ejs = require('ejs');
 const session = require('express-session');
 const { get } = require('https');
+const path = require('path');
+const port = 3000;
+
 
 app.use(express.static('assets'));
+app.use('/js',express.static(path.join(__dirname+'/js')));
+app.use('/css',express.static(path.join(__dirname+'/css')));
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
     secret: 'secret',
@@ -19,5 +24,77 @@ app.get('/', (req, res) => {
         res.send(data);
     })
 })
+app.post('/gamearea', (req, res) => {
 
-server.listen(3000);
+    let user = {
+        nickname: req.body.nickname,
+        roomname: req.body.roomname
+    }
+
+    if (user.nickname == '' || user.roomname == '') {
+        res.redirect('/');
+    } else {
+        session.userName = user.nickname;
+        session.roomName = user.roomname;
+        ejs.renderFile('views/GameArea', { user }, (err, data) => {
+            if (err) throw err;
+            res.send(data);
+        })
+    }
+})
+
+io.on('connection', (socket) => {
+
+    socket.on('joinRoom', () => {
+        const user = userJoin(socket.id, session.userName, session.roomName);
+
+        socket.join(user.room);
+
+        // az új user üdvözlése
+        socket.emit('message', formatMessage('Rendszerüzenet', `Üdvözlünk a ${user.room} szobában!`));
+
+        // a room többi userének elküldeni, hogy valaki csatlakozott
+        socket.broadcast
+            .to(user.room)
+            .emit('message', formatMessage('Rendszerüzenet', `${user.username} csatlakozott a csevegéshez!`));
+
+        // frissítjük a szobában lévő felhasználók listáját
+        io.to(user.room).emit('roomUsers', {
+            users: getRoomUsers(user.room)
+        });
+
+    });
+
+
+    socket.on('disconnect', () => {
+        const user = userLeave(socket.id);
+
+        // a room-nak elküldeni h a user elhagyta a chatet
+        socket.broadcast
+            .to(user.room)
+            .emit('message', formatMessage('Rendszerüzenet', `${user.username} elhagyta a csevegést!`));
+
+        // frissítjük a szobában lévő felhasználók listáját
+        io.to(session.roomName).emit('roomUsers', {
+            users: getRoomUsers(session.roomName)
+        });
+    });
+
+    socket.on('message', msg => {
+
+        const user = getCurrentUser(socket.id);
+
+        // kiküldjük a beérkezett üzenetet mindenkinek a szobában
+        io.to(user.room).emit('message', formatMessage(user.username, msg));
+    });
+
+    socket.on('typing', id => {
+
+        const user = getCurrentUser(socket.id);
+        socket.broadcast.to(user.room).emit('typing', user.id);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`életjel ${port}...`);
+});
